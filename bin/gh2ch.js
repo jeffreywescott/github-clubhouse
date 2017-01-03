c0b0190f8bfeeb26a66d91e9f3bf85215707060e
jeffreywescott <jeffrey@learnersguild.org>
2017-01-02 19:55:24 -0800

    improve argument parsing and configuration

--- bin/gh2ch.js
+++ bin/gh2ch.js
@@ -3,23 +3,35 @@
 var path = require('path')
 var parseArgs = require('minimist')
 
-var config, githubIssueToClubhouseStory
+var ghCh
 try {
-  config = require('github-clubhouse/lib/util/config')
-  githubIssueToClubhouseStory = require('github-clubhouse').githubIssueToClubhouseStory
+  ghCh = require('github-clubhouse')
 } catch (err) {
   // so that we can run during development
-  config = require('../lib/util/config')
-  githubIssueToClubhouseStory = require('../lib').githubIssueToClubhouseStory
+  ghCh = require('../lib')
 }
+var loadConfig = ghCh.loadConfig
+var saveConfig = ghCh.saveConfig
+var githubIssueToClubhouseStory = ghCh.githubIssueToClubhouseStory
 
 function run() {
-  var args = parseArgs(process.argv.slice(2))
-  var options = _loadAndOrSaveOptions(args)
-  if (args._.length !== 2) {
+  var args = parseArgs(process.argv.slice(2), {
+    string: ['github-token', 'clubhouse-token'],
+    boolean: ['s', 'h'],
+    alias: {
+      h: 'help',
+      s: 'save-config',
+    },
+  })
+  if (args.h) {
     console.info(_usage())
-    process.exit(1)
+    return process.exit(0)
   }
+  if (args._.length !== 2) {
+    return _die('GITHUB_ISSUE_URL and CLUBHOUSE_PROJECT_NAME are both required.')
+  }
+
+  var options = _loadAndOrSaveOptions(args)
 
   githubIssueToClubhouseStory(args._[0], args._[1], options)
     .then(function(story) {
@@ -30,29 +42,36 @@ function run() {
     })
 }
 
+function _die(message) {
+  console.error(`ERROR: ${message}`)
+  console.error('Try `--help` for help.')
+  process.exit(1)
+}
+
 function _usage() {
   return 'Usage:\n' +
     '  ' + path.basename(process.argv[1]) + ' GITHUB_ISSUE_URL CLUBHOUSE_PROJECT_NAME\n' +
     '\n' +
     'Options:\n' +
+    '  -h, --help                 display this help message\n' +
+    '  -s, --save-config          save configuration into ~/.github-clubhouse\n' +
     '  --github-token=TOKEN       your GitHub API token\n' +
-    '  --clubhouse-token=TOKEN    your Clubhouse API token\n' +
-    '  --save-config              save configuration into ~/.github-clubhouse'
+    '  --clubhouse-token=TOKEN    your Clubhouse API token'
 }
 
 function _loadAndOrSaveOptions(args) {
-  var options = config.loadConfig()
+  var options = loadConfig()
   if (args['github-token']) {
     options.githubToken = args['github-token']
   }
   if (args['clubhouse-token']) {
     options.clubhouseToken = args['clubhouse-token']
   }
-  if (Object.keys(options).length > 0) {
-    config.createConfigIfNotExists(options)
-  }
-  if (args['save-config']) {
-    config.saveConfig(options)
+  if (args.s) {
+    if (!args['github-token'] || !args['clubhouse-token']) {
+      return _die('Cannot save configuration unless both `--github-token` and `--clubhouse-token` are provided.')
+    }
+    saveConfig(options)
   }
 
   return options
