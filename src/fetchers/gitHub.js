082a700dc8c30e14760c467b548b166094464dbf
jeffreywescott <jeffrey@learnersguild.org>
2017-07-07 14:39:25 -0700

    add endpoints to create GitHub issues and issue comments

--- src/fetchers/gitHub.js
+++ src/fetchers/gitHub.js
@@ -20,3 +20,21 @@ export function getCommentsForIssue(token, owner, repoName, issueNumber) {
   const commentsUrl = apiURL(`/repos/${owner}/${repoName}/issues/${issueNumber}/comments`)
   return apiFetchAllPages(commentsUrl, {headers: headers(token)})
 }
+
+export function createIssue(token, owner, repoName, issue) {
+  const newIssueUrl = apiURL(`/repos/${owner}/${repoName}/issues`)
+  return apiFetch(newIssueUrl, {
+    method: 'POST',
+    headers: headers(token),
+    body: JSON.stringify(issue),
+  })
+}
+
+export function createIssueComment(token, owner, repoName, issueNumber, comment) {
+  const newCommentUrl = apiURL(`/repos/${owner}/${repoName}/issues/${issueNumber}/comments`)
+  return apiFetch(newCommentUrl, {
+    method: 'POST',
+    headers: headers(token),
+    body: JSON.stringify(comment),
+  })
+}
